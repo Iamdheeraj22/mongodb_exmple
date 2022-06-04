34e9919c4fa7a6b3057c519d1d441551c274f036
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-06-04 22:48:38 +0530

    first commit

---
+++ src/test/java/com/mongodb/mongodb_exmple/MongodbExmpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongodb.mongodb_exmple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbExmpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
